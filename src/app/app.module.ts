6c3873e41ae546e67522e27dd76de53d26523d2d
Enrico PIccinin <enrico.piccinin@gmail.com>
2019-06-28 08:35:58 +0200

    fix(app.module.ts): JWT_OPTIONS added in the import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { JwtModule } from '@auth0/angular-jwt';
+import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
 
 import { AppMaterialModule } from './app-material.module';
 import { AppComponent } from './app.component';
